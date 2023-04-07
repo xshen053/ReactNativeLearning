e53c5948d2e3a0f6c61c53603bd4f5c5505916da
xshen053 <shenxiaxi26@gmail.com>
2023-04-06 23:13:17 -0700

    initialized demo

---
+++ style_demo/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Index from './src_01_StyleSheet';
+export default class App extends Component {
+  render() {
+    return <Index />;
+  }
+}
