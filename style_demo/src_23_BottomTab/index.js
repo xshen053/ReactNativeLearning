c3b6fbb7b45e65096d1bcf72c9abfb37983c3137
xshen053 <shenxiaxi26@gmail.com>
2023-04-09 15:51:31 -0700

    configure xcode to support RNIcons

--- style_demo/src_23_BottomTab/index.js
+++ style_demo/src_23_BottomTab/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View, Button} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 function HomeScreen(prop) {
   return (
     <View style={[styles.container]}>
@@ -21,19 +22,18 @@ export default class index extends Component {
   render() {
     return (
       <Tab.Navigator
-      // screenOptions={route => ({
-      //   tabBarIcon: ({focused, color, size}) => {
-      //     let iconName;
-
-      //     if (route.name === 'Home') {
-      //       iconName = 'Home';
-      //     } else if (route.name === 'News') {
-      //       iconName = 'News';
-      //     }
-      //     return <Button title="Hello" size={size} color={color} />;
-      //   },
-      // })}
-      >
+        // ({route}) != route
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            } else if (route.name === 'News') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="News" component={NewsScreen} />
       </Tab.Navigator>
